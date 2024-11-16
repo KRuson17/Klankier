1bf3050f62c466b7c609ded691f569412b4d8634
KRuson17 <148062167+KRuson17@users.noreply.github.com>
2024-11-16 22:54:59 +0300

    platforma

---
+++ script/platform.gd
@@ -0,0 +1,12 @@
+extends StaticBody2D
+
+var rangeToMove = [0, 200]
+var direc = true
+var step = 1
+
+func _process(_delta: float) -> void:
+	if abs(rangeToMove[0]) >= abs(rangeToMove[1]):
+		direc = !direc
+	step = 1 if direc else -1
+	position += Vector2(step, 0)
+	rangeToMove[0] += step
