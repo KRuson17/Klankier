7453d0aa04ea9b00193b8ae36aaca7f22cbc2b55
KRuson17 <148062167+KRuson17@users.noreply.github.com>
2024-11-16 21:01:58 +0300

    kardowanie thorna

--- script/portal.gd
+++ script/portal.gd
@@ -1,11 +1,11 @@
 extends Sprite2D
 
-
+@export var speed = 2
 
 func _ready() -> void:
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	rotation_degrees += 1 if rotation_degrees >= 360 else 1
+func _process(_delta: float) -> void:
+	rotation_degrees += speed if rotation_degrees >= 360 else speed
