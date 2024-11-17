692577ecf2cbac5a90d48f20ffe06d920c1e3ee8
KRuson17 <148062167+KRuson17@users.noreply.github.com>
2024-11-17 14:22:26 +0300

    zapętlenie muzyki

--- script/music.gd
+++ script/music.gd
@@ -3,9 +3,15 @@ extends AudioStreamPlayer
 
 var current_scene_name: String = "menu" # Domyślna scena
 
+
 func play_music(new_stream: AudioStream, scene_name: String) -> void:
 	if scene_name != current_scene_name:
 		current_scene_name = scene_name
 		stop()
 		self.stream = new_stream  # Ustawiamy strumień z nową muzyką
 		play()
+	
+
+
+func _on_finished() -> void:
+	play()
